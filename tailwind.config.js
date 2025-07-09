b9ba6673d3df7eccbbde282a921c92d61e13d174
yxvngmun <munaeb2005@gmail.com>
2025-07-09 22:30:46 +0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JS, JSX, TS, TSX files in the src directory
-    "./public/index.html",       // Scans your main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",       
   ],
   theme: {
     extend: {},
